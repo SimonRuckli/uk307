e09b3a7686fa8148485371913b9ac9265553fad8
SimonRuckli <55438072+SimonRuckli@users.noreply.github.com>
2020-05-05 18:15:01 +0200

    Update task.view.php

--- Reparaturwerkstatt/app/Views/task.view.php
+++ Reparaturwerkstatt/app/Views/task.view.php
@@ -1,2 +1,38 @@
-<?php
-echo("hi xD");
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <title>Reparaturwerkstatt</title>
+        <link rel="stylesheet" href="public/css/css.css">
+    </head>
+    <body>
+        <form action="validation" method="post">
+
+                <lable for="name">Name</lable><br>
+                <input name="name" type="text" id="name" require><br>
+
+                <lable for="email">E-Mail</lable><br>
+                <input name="email" type="email" id="email" require><br>
+
+                <lable for="phone">Telefon</lable><br>
+                <input name="phone" type="text" id="phone"><br>
+
+                <lable>Dringlichkeit</lable><br>
+                <select name="urgency" require>
+                    <option value=""></option>
+                    <option value="very_deep">Sehr tief</option>
+                    <option value="deep">Tief</option>
+                    <option value="normal">Normal</option>
+                    <option value="high">Hoch</option>
+                    <option value="very_high">Sehr hoch</option>
+                </select><br>
+
+                <lable for="tool">Betreffendes Werkzeug</lable><br>
+                <input name="tool" type="text" id="tool" require><br>
+
+            <button name="open">Auftragsliste öffnen</button>
+            <button name="add">Auftrag hinzufügen</button>
+
+        </form>
+    </body>
+</html>
