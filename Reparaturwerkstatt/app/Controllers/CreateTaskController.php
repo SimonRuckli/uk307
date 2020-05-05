5b941923b2bfd57f040a9ddb103c0ae343918d03
SnowyWhite <45012141+SnowyWhite@users.noreply.github.com>
2020-05-05 17:04:39 +0200

    Erste änderungen

---
+++ Reparaturwerkstatt/app/Controllers/CreateTaskController.php
@@ -0,0 +1,2 @@
+<?php
+require 'app/Views/task.view.php';
--- sql/workshop.sql
+++ sql/workshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2020 at 04:04 PM
+-- Generation Time: May 05, 2020 at 05:03 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `werkstatt`
+-- Database: `workshop`
 --
 
 -- --------------------------------------------------------
