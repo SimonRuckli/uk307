9cd9ac171c364c74ad4f52ce852564daf126d82b
joegijoegi <58853970+joegijoegi@users.noreply.github.com>
2020-05-06 13:48:37 +0200

    add validation

--- Reparaturwerkstatt/app/Controllers/ProcessTaskController.php
+++ Reparaturwerkstatt/app/Controllers/ProcessTaskController.php
@@ -4,19 +4,23 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     if ($_POST["button"] == "add") {
 
-        // Formularfelder auslesen
-        $dto = array (
-            "name"    => post("name"),
-            "email"   => post("email"),
-            "phone"   => post("phone"),
-            "daysUntilReturn" => filter_var(post("urgencydays"), FILTER_SANITIZE_NUMBER_INT),
-            "urgency" => post("urgency"),
-            "tool"    => post("tool")
-        );
-      
-        // Datenbank-Objekt erstellen
-        $task = new Task($dto);
-        $task->addToDatabase();
+        validateName();
+        if(count(Validation::$errors) === 0)
+        {
+            // Formularfelder auslesen
+            $dto = array (
+                "name"    => post("name"),
+                "email"   => post("email"),
+                "phone"   => post("phone"),
+                "daysUntilReturn" => filter_var(post("urgencydays"), FILTER_SANITIZE_NUMBER_INT),
+                "urgency" => post("urgency"),
+                "tool"    => post("tool")
+            );
+        
+            // Datenbank-Objekt erstellen
+            $task = new Task($dto);
+            $task->addToDatabase();
+        }
 
         header("Location: addtask");
     } elseif ($_POST["button"]  == "tasklist") {
