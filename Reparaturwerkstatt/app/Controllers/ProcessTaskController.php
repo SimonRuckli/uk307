ef917cc7ffed1d0fc4721afe90f30ba05a677674
SimonRuckli <55438072+SimonRuckli@users.noreply.github.com>
2020-05-06 13:12:25 +0200

    Update ProcessTaskController.php

--- Reparaturwerkstatt/app/Controllers/ProcessTaskController.php
+++ Reparaturwerkstatt/app/Controllers/ProcessTaskController.php
@@ -5,27 +5,3 @@ $tools = $dto->getallTools();
 echo("<pre>");
 print_r($tools);
 echo("</pre>");
-
-
-// if ($_SERVER["REQUEST_METHOD"] === "POST") {
-
-//     if ($_POST["button"] == "add") {
-
-//         // Formularfelder auslesen
-//         $dto = array (
-//             "name"    => post("name"),
-//             "email"   => post("email"),
-//             "phone"   => post("phone"),
-//             "urgency" => post("urgency"),
-//             "tool"    => post("tool")
-//         );
-      
-//         // Datenbank-Objekt erstellen
-//         $task = new Task($dto);
-//         $task->addToDatabase();
-
-//         header("Location: addtask");
-//     } elseif ($_POST["button"]  == "tasklist") {
-//         header("Location: tasklist");
-//     }
-// }
